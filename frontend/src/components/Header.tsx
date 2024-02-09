e182c4ad14bcbad9374237ac9376f076f7febf8e
ManoharJunga <manohar_junga@srmap.edu.in>
2024-02-09 22:18:36 +0530

    frontend

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,9 +2,13 @@ import AppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
 
 const Header = () => {
-  return <AppBar sx={{}}>
-    <Toolbar></Toolbar>
-  </AppBar>;
+  return ( 
+  <AppBar 
+    sx={{ bgcolor: "transparent", position: "static", boxShadow: "none"}}
+  >
+    <Toolbar sx={ {display: "flex" }}></Toolbar>
+  </AppBar>
+  );
 }
 
 export default Header

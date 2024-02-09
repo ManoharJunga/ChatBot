bed6ecbcb4a89111a3502b997f18f33e6fca97cd
ManoharJunga <manohar_junga@srmap.edu.in>
2024-02-10 00:21:00 +0530

    login

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
 
+import { Typography } from "@mui/material";
+import {Link } from "react-router-dom";
 const Logo = () => {
-  return (
-    <div>Logo</div>
-  )
+  return( <div
+    style={{
+        display: "flex",
+        marginRight: "auto",
+        alignItems: "center",
+        gap: "8px",
+    }}
+  >
+    <Link to={"/"}>
+        <img src='openai.png' alt="openai" width={"30px"} height={"30px"} className="image-inverted"
+        /> 
+        <Typography sx={{ display: {md: "block", sm: "none", xs: "none"}, mr:"auto", fontWeight: "800", textShadow: "2px 2px 20px #000",}}>
+            <span style={{ fontSize: "20px" }}>MERN</span>
+        </Typography>
+    </Link>
+  </div>
+  );
 }
-
 export default Logo

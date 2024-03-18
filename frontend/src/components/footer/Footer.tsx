e2a1b3adc8397af6008fb69e72bc4f3694c1c753
ManoharJunga <manohar_junga@srmap.edu.in>
2024-03-18 16:53:31 +0530

    commit

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 
 const Footer = () => {
   return (
@@ -8,8 +9,50 @@ const Footer = () => {
           minHeight: "20vh",
           maxHeight: "30vh",
           marginTop: 60,
+          backgroundColor: "#f0f0f0",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          padding: "20px 0",
         }}
       >
+        <h2 style={{ color: "#333", marginBottom: 10 }}>Stay Connected</h2>
+        <p style={{ color: "#666", fontSize: 16 }}>
+          Follow us on social media for the latest updates and news.
+        </p>
+        <div style={{ marginTop: 20 }}>
+          <a
+            href="#"
+            style={{
+              marginRight: 10,
+              color: "#007bff",
+              textDecoration: "none",
+            }}
+          >
+            Facebook
+          </a>
+          <a
+            href="#"
+            style={{
+              marginRight: 10,
+              color: "#007bff",
+              textDecoration: "none",
+            }}
+          >
+            Twitter
+          </a>
+          <a
+            href="#"
+            style={{
+              marginRight: 10,
+              color: "#007bff",
+              textDecoration: "none",
+            }}
+          >
+            Instagram
+          </a>
+        </div>
       </div>
     </footer>
   );
--- frontend/src/components/shared/Details.tsx
+++ frontend/src/components/shared/Details.tsx
@@ -29,6 +29,7 @@ const Details: React.FC = () => {
   };
 
   const cardStyles: React.CSSProperties = {
+    marginTop: '100px',
     margin: '40px',
     position: 'relative',
     width: '300px',
@@ -87,9 +88,11 @@ const Details: React.FC = () => {
         onMouseEnter={handleMouseEnter1}
         onMouseLeave={handleMouseLeave1}
       >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style={svgStyles}>
-          <path d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path>
-        </svg>
+        <img
+            src="symptons.png"
+            alt="symptons"
+            style={{ width: "250px",}}
+          />
         <div style={{ ...contentStyles, transform: isHovered1 ? 'rotateX(0)' : 'rotateX(-90deg)' }} className="card__content">
           <p style={titleStyles} className="card__title">Symptom Assessment</p>
           <p style={descriptionStyles} className="card__description">Describe your symptoms, and our chatbot will assist you in evaluating them.</p>
@@ -102,9 +105,11 @@ const Details: React.FC = () => {
         onMouseEnter={handleMouseEnter2}
         onMouseLeave={handleMouseLeave2}
       >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style={svgStyles}>
-          <path d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path>
-        </svg>
+        <img
+            src="Analysis.png"
+            alt="Analysis"
+            style={{ height: "200px",}}
+          />
         <div style={{ ...contentStyles, transform: isHovered2 ? 'rotateX(0)' : 'rotateX(-90deg)' }} className="card__content">
           <p style={titleStyles} className="card__title">Check Symptoms</p>
           <p style={descriptionStyles} className="card__description">Get information about symptoms and possible conditions.</p>
@@ -116,9 +121,11 @@ const Details: React.FC = () => {
         onMouseEnter={handleMouseEnter3}
         onMouseLeave={handleMouseLeave3}
       >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style={svgStyles}>
-          <path d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path>
-        </svg>
+        <img
+            src="information.webp"
+            alt="information"
+            style={{ width: "300px",}}
+          />
         <div style={{ ...contentStyles, transform: isHovered3 ? 'rotateX(0)' : 'rotateX(-90deg)' }} className="card__content">
           <p style={titleStyles} className="card__title">Medical Information</p>
           <p style={descriptionStyles} className="card__description">Access reliable medical information and resources.</p>
